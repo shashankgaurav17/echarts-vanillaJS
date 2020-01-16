8a8b2704126fc39325fbf3889fbc7053a05eea24
Shashank Gaurav <shashank@cyware.com>
2020-01-16 13:18:25 +0530

    init

---
+++ data/bar-data.js
@@ -0,0 +1,79 @@
+export default {
+  data: [
+    {
+      filters: { threat_method: "388f9c71-c96c-4d06-993f-a8bbaf7726ee" },
+      label: "User Account Compromise",
+      value: 0
+    },
+    {
+      filters: { threat_method: "b50f9b62-4ba8-4077-8117-4409275add83" },
+      label: "Network Scanning",
+      value: 0
+    },
+    {
+      filters: { threat_method: "16abd16d-280c-4141-b234-f108aabcb739" },
+      label: "Spearphishing",
+      value: 1
+    },
+    {
+      filters: { threat_method: "956db0b5-e94a-4a97-8a8f-c0a10d2bc376" },
+      label: "Spamming",
+      value: 1
+    },
+    {
+      filters: { threat_method: "1d6fb95d-28c5-405d-a495-53b04f1be828" },
+      label: "Error",
+      value: 0
+    },
+    {
+      filters: { threat_method: "6bd847b4-0b1c-4e00-a006-2cc9fc9b706a" },
+      label: "Hacking",
+      value: 0
+    },
+    {
+      filters: { threat_method: "896bec40-9be1-458f-9eda-90333bc385db" },
+      label: "IP Spoofing",
+      value: 0
+    },
+    {
+      filters: { threat_method: "32d1f9d9-2961-4a37-8647-d167c61d81e0" },
+      label: "Lost Device",
+      value: 0
+    },
+    {
+      filters: { threat_method: "c5d838ea-b2cb-4057-a618-b1817de6289a" },
+      label: "Malware",
+      value: 0
+    },
+    {
+      filters: { threat_method: "fe4eba87-a497-4095-8da7-5a8b97656ade" },
+      label: "Phishing",
+      value: 0
+    },
+    {
+      filters: { threat_method: "30fc6373-250d-48fb-ac2a-53256ed9caea" },
+      label: "Website Defacement",
+      value: 1
+    },
+    {
+      filters: { threat_method: "2c4ed80c-a2ca-45fc-ab76-d33f24df92d7" },
+      label: "System Misuse",
+      value: 0
+    },
+    {
+      filters: { threat_method: "7de0603a-f18e-47b6-a6c6-557fa69f3c12" },
+      label: "APT",
+      value: 0
+    },
+    {
+      filters: { threat_method: "d5f462c9-be6a-435e-a205-d35ff208820a" },
+      label: "Improper Disposal",
+      value: 0
+    },
+    {
+      filters: { threat_method: "de4a7183-fe7e-4c51-b426-987c38633327" },
+      label: "Other",
+      value: 0
+    }
+  ]
+};
